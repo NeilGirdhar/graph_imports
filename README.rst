e184b4bc108079a33c6d8641bc0ea6b666ef3055
Neil Girdhar <mistersheik@gmail.com>
2021-05-11 15:46:15 -0400

    Initial commit

---
+++ README.rst
@@ -0,0 +1,24 @@
+=============
+Graph Imports
+=============
+
+.. role:: bash(code)
+    :language: bash
+
+.. role:: python(code)
+   :language: python
+
+This repository allows one to graph the imports between packages.
+
+-----------------------
+Contribution guidelines
+-----------------------
+
+- Conventions: PEP8.
+
+- How to clean the source:
+
+  - :bash:`isort tjax`
+  - :bash:`pylint tjax`
+  - :bash:`mypy tjax`
+  - :bash:`flake8 tjax`
---
+++ graph_imports/__init__.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+
+import subprocess
+from collections.abc import Mapping
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import typer
+
+__all__ = ['cli']
+
+
+cli = typer.Typer()
+
+
+def find_modules(path: Path,
+                 path_components: list[str]) -> Mapping[str, list[str]]:
+    modules: dict[str, list[str]] = {}
+    ruined_module = '_'.join(path_components)
+    if path.is_dir():
+        modules[ruined_module] = (path_components[1:]
+                                  if len(path_components) > 1
+                                  else path_components)
+        for sub_path in path.iterdir():
+            modules.update(find_modules(sub_path, path_components + [sub_path.stem]))
+    elif path.is_file() and path.suffix == '.py':
+        if path.stem != '__init__':
+            modules[ruined_module] = (path_components[1:-1]
+                                      if len(path_components) > 2
+                                      else path_components[:-1])
+    return modules
+
+
+def shorten(name: str,
+            modules: Mapping[str, list[str]]) -> str:
+    retval = '•'.join(modules[name.strip()])
+    if retval in ['graph', 'edge']:
+        return f'{retval}_'
+    return retval
+
+
+def attrs(fmt: str) -> dict[str, str]:
+    return dict(kv.split('=') for kv in fmt.strip()[:-2].split(','))  # type: ignore
+
+
+def attrs2fmt(attr_map: Mapping[str, str]) -> str:
+    return '[%s];' % ','.join('%s=%s' % (k, v) for k, v in attr_map.items())
+
+
+@cli.command()
+def main(base_name: str) -> None:
+    modules = find_modules(Path(base_name), [base_name])
+
+    cp = subprocess.run(['pydeps', base_name, '--max-bacon=1', '--show-dot', '--no-output'],
+                        stdout=subprocess.PIPE, text=True, check=True)
+    lines = cp.stdout.splitlines()
+    header = [line
+              for line in lines[:6]
+              if 'concentrate' not in line and line != '']
+    body = lines[6:-3]
+    nodes = [line for line in body if '->' not in line if line]
+
+    node_mapping: dict[str, dict[str, str]] = {}
+    for node in nodes:
+        name, fmt = node.split('[')
+        sname = shorten(name, modules)
+        if sname in node_mapping:
+            continue
+        node_mapping[sname] = attrs(fmt)
+
+    rules = [line for line in body if '->' in line]
+
+    rule_mapping: dict[tuple[str, str], dict[str, str]] = {}
+    used_nodes = set()
+    for rule in rules:
+        arrow, fmt = rule.split('[')
+
+        a, _, b = arrow.split()
+        a = shorten(a, modules)
+        b = shorten(b, modules)
+
+        if (a, b) in rule_mapping:
+            continue
+        if a == b:
+            continue
+        if b == base_name:
+            continue
+        rule_mapping[(a, b)] = attrs(fmt)
+
+        used_nodes.add(a)
+        used_nodes.add(b)
+
+    with NamedTemporaryFile('w') as fp:
+        fp.write('\n'.join(header))
+        for n in used_nodes:
+            some_dict: dict[str, str] = node_mapping[n]
+            some_dict['label'] = '"%s"' % n
+            print('    {} {}'.format(n, attrs2fmt(some_dict)), file=fp)
+        for (a, b), fmt_dict in rule_mapping.items():
+            print('    {} -> {} {}'.format(a, b, attrs2fmt(fmt_dict)), file=fp)
+        print('}', file=fp)
+        fp.flush()
+
+        uml_dir = Path('uml')
+        write_to = (f'uml/{base_name}.png'
+                    if uml_dir.exists() and uml_dir.is_dir()
+                    else f'{base_name}.png')
+        subprocess.run(['dot', '-Tpng', fp.name, '-o', write_to], check=True)
