5c83d0681563623c89dd2f0f25a0a6f5a3cb8ca6
Neil Girdhar <mistersheik@gmail.com>
2024-09-23 22:46:13 -0400

    Fix uv configuration

--- graph_imports/__init__.py
+++ graph_imports/__init__.py
@@ -55,10 +55,7 @@ def parse_dependencies(base_name: str,
                        ) -> tuple[Sequence[str], Mapping[str, dict[str, str]], Sequence[str]]:
     string_io = io.StringIO()
     with redirect_stdout(string_io):
-        pydeps.call_pydeps(base_name,
-                           max_bacon=1,
-                           show_dot=True,
-                           no_output=True)
+        _ = pydeps.call_pydeps(base_name, max_bacon=1, show_dot=True, no_output=True)
     string_value = string_io.getvalue()
     lines = string_value.splitlines()
     header = [line
@@ -105,14 +102,14 @@ def main(base_name: str) -> None:
         used_nodes.add(b)
 
     dot_stringio = io.StringIO()
-    dot_stringio.write('\n'.join(header))
+    _ = dot_stringio.write('\n'.join(header))
     for n in used_nodes:
         some_dict: dict[str, str] = node_mapping[n]
         some_dict['label'] = f'"{n}"'
-        dot_stringio.write(f'    {n} {attrs2fmt(some_dict)}')
+        _ = dot_stringio.write(f'    {n} {attrs2fmt(some_dict)}')
     for (a, b), fmt_dict in rule_mapping.items():
-        dot_stringio.write(f'    {a} -> {b} {attrs2fmt(fmt_dict)}')
-    dot_stringio.write('}')
+        _ = dot_stringio.write(f'    {a} -> {b} {attrs2fmt(fmt_dict)}')
+    _ = dot_stringio.write('}')
     output = pydeps.dot.call_graphviz_dot(dot_stringio.getvalue(), 'png')
 
     uml_dir = Path('uml')
@@ -120,4 +117,4 @@ def main(base_name: str) -> None:
                 if uml_dir.exists() and uml_dir.is_dir()
                 else Path(f'{base_name}.png'))
     with write_to.open('wb') as f:
-        f.write(output)
+        _ = f.write(output)
